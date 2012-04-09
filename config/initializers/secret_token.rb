297bd19291824407482d0bee9b88befc33845782
Ed van Beinum <ed@vanbeinum.org>
2012-04-09 16:27:27 +0100

    remove TestUnit and use Rspec instead

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-HardrefreshDevelopmentLocal::Application.config.secret_token = 'a3f128cb286d0785ce9820cf65aa8c5b3ccc62b27903f527287ccdcd907be37204f6fe1db7c593139119ea7e2c03c7102d901f48c347a1756880920b3ef1556d'
+HardrefreshDevelopmentLocal::Application.config.secret_token = '3076ba49b48ba65a216aa423881e4f2407e4a9f8b403086c6c81ba8c45e45b230fffeb49751018d81b03894b83f4cb44dd4165c037913315dacf9b11a75a023d'

5c4f2dcd404010bac3d970a397482855a845ff28
Ed van Beinum <ed@vanbeinum.org>
2012-04-09 15:47:01 +0100

    create default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HardrefreshDevelopmentLocal::Application.config.secret_token = 'a3f128cb286d0785ce9820cf65aa8c5b3ccc62b27903f527287ccdcd907be37204f6fe1db7c593139119ea7e2c03c7102d901f48c347a1756880920b3ef1556d'

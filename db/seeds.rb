578487e58b707b0ed8833881ad9e89130b102a03
Ed van Beinum <ed@vanbeinum.org>
2012-04-27 21:38:44 +0100

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,101 +1,90 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-["Firefox", "Safari", "Chrome", "Internet Explorer", "Opera"].each do |browser|
-  Browser.find_or_create_by_name(browser)
+browsers = Hash.new
+
+["firefox", "safari", "chrome", "ie", "opera"].each do |browser|
+  new_browser = Browser.find_or_create_by_name(browser)
+  browsers[browser] = new_browser.id
 end
 
-["Mac OSX", "Linux", "Windows"].each do |platform|
-  Platform.find_or_create_by_name(platform)
+platforms = Hash.new
+
+["macintosh", "linux", "windows"].each do |platform|
+  new_platform = Platform.find_or_create_by_name(platform)
+  platforms[platform] = new_platform.id
 end
 
 Instruction.delete_all
 
-mac_osx = 1
-linux = 2
-windows = 3
-
-firefox = 1
-safari = 2
-chrome = 3
-ie = 4
-opera = 5
-
 Instruction.create(
-  platform_id: mac_osx,
-  browser_id: firefox,
+  platform_id: platforms['macintosh'],
+  browser_id: browsers['firefox'],
   content: "1. <span class='keyboard-key'>Cmd</span> + <span class='keyboard-key'>Shift</span> + <span class='keyboard-key'>Backspace</span>
 2. Select '_Everything_' from the dropdown box and make sure the '_Cache_' checkbox is ticked.
 3. Then click '_Clear Now_'"
 )
 
 Instruction.create(
-  platform_id: linux,
-  browser_id: firefox,
+  platform_id: platforms['linux'],
+  browser_id: browsers['firefox'],
   content: "1. <span class='keyboard-key'>Ctrl</span> + <span class='keyboard-key'>Shift</span> + <span class='keyboard-key'>Delete</span> and
 2. Select '_Everything_' from the dropdown box and make sure the '_Cache_' checkbox is ticked.
 3. Then click '_Clear Now_'"
 )
 
 Instruction.create(
-  platform_id: windows,
-  browser_id: firefox,
+  platform_id: platforms['windows'],
+  browser_id: browsers['firefox'],
   content: "1. <span class='keyboard-key'>Ctrl</span> + <span class='keyboard-key'>Shift</span> + <span class='keyboard-key'>Delete</span>
 2. Select '_Everything_' from the dropdown box and make sure the '_Cache_' checkbox is ticked
 3. Then click '_Clear Now_'"
 )
 
 Instruction.create(
-  platform_id: mac_osx,
-  browser_id: safari,
+  platform_id: platforms['macintosh'],
+  browser_id: browsers['safari'],
   content: "1. <span class='keyboard-key'>Alt</span> + <span class='keyboard-key'>Cmd</span>, <span class='keyboard-key'>E</span>
 2. Then click '_Clear_'"
 )
 
 Instruction.create(
-  platform_id: windows,
-  browser_id: safari,
+  platform_id: platforms['windows'],
+  browser_id: browsers['safari'],
   content: "1. <span class='keyboard-key'>Ctrl </span>+ <span class='keyboard-key'>Alt</span> + <span class='keyboard-key'>E</span>
 2. Click '_Empty_'"
 )
 
 Instruction.create(
-  platform_id: mac_osx,
-  browser_id: chrome,
+  platform_id: platforms['macintosh'],
+  browser_id: browsers['chrome'],
   content: "1. <span class='keyboard-key'>Cmd</span> + <span class='keyboard-key'>Shift</span> + <span class='keyboard-key'>Backspace</span>
 2. Then click '_Clear Browsing Data_'"
 )
 
 Instruction.create(
-  platform_id: linux,
-  browser_id: chrome,
+  platform_id: platforms['linux'],
+  browser_id: browsers['chrome'],
   content: "1. <span class='keyboard-key'>Ctrl</span> + <span class='keyboard-key'>Shift</span> + <span class='keyboard-key'>Delete</span> and
 2. Select '_Everything_' from the dropdown box and make sure the '_Cache_' checkbox is ticked.
 3. Then click '_Clear Now_'"
 )
 
 Instruction.create(
-  platform_id: windows,
-  browser_id: chrome,
+  platform_id: platforms['windows'],
+  browser_id: browsers['chrome'],
   content: "1. <span class='keyboard-key'>Ctrl</span>  + <span class='keyboard-key'>Shift</span>  + <span class='keyboard-key'>Del</span>
 2. Click '_Clear browsing data_'"
 )
 
 Instruction.create(
-  platform_id: windows,
-  browser_id: ie,
+  platform_id: platforms['windows'],
+  browser_id: browsers['ie'],
   content: "1. <span class='keyboard-key'>Ctrl</span> + <span class='keyboard-key'>Shift</span> + <span class='keyboard-key'>Delete</span>
 2. Make sure '_Temporary Internet files_' is checked
 3. Click '_Delete_'"
 )
 
 Instruction.create(
-  platform_id: mac_osx,
-  browser_id: opera,
+  platform_id: platforms['macintosh'],
+  browser_id: browsers['opera'],
   content: "1. <span class='keyboard-key'>Cmd</span> + <span class='keyboard-key'>,</span>
 2. Click '_Advanced_'
 3. Click on -_History_- on the left hand menu
@@ -104,8 +93,8 @@ Instruction.create(
 )
 
 Instruction.create(
-  platform_id: linux,
-  browser_id: opera,
+  platform_id: platforms['linux'],
+  browser_id: browsers['opera'],
   content: "1. Click '_Preferences_' from the '_Tools_' menu
 2. Click '_Advanced_'
 3. Click on -_History_- on the left hand menu
@@ -114,8 +103,8 @@ Instruction.create(
 )
 
 Instruction.create(
-  platform_id: windows,
-  browser_id: opera,
+  platform_id: platforms['windows'],
+  browser_id: browsers['opera'],
   content: "1. <span class='keyboard-key'>Ctrl</span>  + <span class='keyboard-key'>F12</span>
 2. Click '_Advanced_'
 3. Click on -_History_- on the left hand menu

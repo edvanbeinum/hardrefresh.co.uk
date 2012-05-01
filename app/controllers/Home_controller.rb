f7e61501634287e203bd383ab87f77d134ecb634
Ed van Beinum <ed@vanbeinum.org>
2012-05-01 22:01:41 +0100

    update USeragent output and add browser icon to instruction page

--- app/controllers/Home_controller.rb
+++ app/controllers/Home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     ua = AgentOrange::UserAgent.new request.env["HTTP_USER_AGENT"]
-    @browser = ua.device.engine.browser.type.to_s.downcase
-    @platform = ua.device.platform.to_s.downcase
+    @browser = ua.device.engine.browser.type.to_s
+    @platform = ua.device.platform.to_s
 
-    @instruction = Instruction.new.get_instructions(@browser, @platform)
+    @instruction = Instruction.new.get_instructions(@browser.downcase, @platform.downcase)
   end
 end
